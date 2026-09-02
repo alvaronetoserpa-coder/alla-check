b4141b3f9cea676b52be7e001da87c762a3b05d7
alvaronetoserpa-coder <alvaroneto.serpa@gmail.com>
2026-09-02 16:21:06 -0300

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AllaCheckApp from "./alla-check.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AllaCheckApp />
-  </React.StrictMode>
-);
